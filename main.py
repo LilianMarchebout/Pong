bf9188e56353a222fb1cc1407b29f554b99bb4ed
Lilian <liliannalil347@gmail.com>
2021-07-02 18:18:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pygame
 from pygame.locals import *
-import raquette
+import raquette as r
 import constantes as c
-import game
-import balle
+import game as g
+import balle as b
 
 pygame.init()
 
 ecran = pygame.display.set_mode(c.dimension_ecran)
+raquette = r.Raquette((0,0),(c.largueur_raquette,c.longueur_raquette))
 
-
-while c.continuer:
+while c.continuer: 
+    raquette.show(ecran)
     for event in pygame.event.get():
         if event.type == QUIT:
             continuer = False
