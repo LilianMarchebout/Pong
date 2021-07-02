88b9ff28fb458cbae896d1f272b0adc3297bd037
Lilian <liliannalil347@gmail.com>
2021-07-02 21:59:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -15,16 +15,20 @@ balle = b.Balle(c.coordonnes_balle_initiales, (c.taille_balle,c.taille_balle))
 
 while c.continuer: 
     p.draw.rect(ecran, c.couleur_noire, (0,0,c.dimension_ecran[0],c.dimension_ecran[1])) #Fond
-    raquette.show(ecran)
-    balle.show(ecran)
     for event in p.event.get():
         if event.type == p.QUIT:
             c.continuer = False
+        #Action de la raquette
         if event.type == p.KEYDOWN:
             if event.key == p.K_UP:
                 raquette.up(c.vitesse_raquette)
             if event.key == p.K_DOWN:
                 raquette.down(c.vitesse_raquette)
+    #Action de la balle
+    balle.deplacement()
+    #Actualise
+    raquette.show(ecran)
+    balle.show(ecran)
     p.display.flip()
     time.sleep(0.01)
     
