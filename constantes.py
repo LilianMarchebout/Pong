4f14ae3acecc5faa06a78846e461aac631f966f6
Lilian <liliannalil347@gmail.com>
2021-07-02 18:38:50 +0200

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -4,5 +4,5 @@ vitesse_raquette = 10
 vitesse_balle = 20
 longueur_raquette = 60
 largueur_raquette = 5
-couleur_blanc = (255,255,255)
-couleur_noire = (0,0,0)
+couleur_blanc = 255,255,255
+couleur_noire = 0,0,0
