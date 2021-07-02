e982203f208c435a27677ee1b83ab4548ace2778
Lilian <liliannalil347@gmail.com>
2021-07-02 18:18:00 +0200

    Update raquette.py

--- raquette.py
+++ raquette.py
@@ -5,7 +5,7 @@ class Raquette:
         self.dx, self.dy = longueurs[0], longueurs[1]
     def show(self, ecran):
         """Affiche la raquette à l'écran"""
-        pygame.display.rect(ecran, (0,0,0), (self.x,self.y,self.dx,self.dy))
+        pygame.draw.rect(ecran, (0,0,0), (self.x,self.y,self.dx,self.dy))
     def up(self, vitesse):
         """Change ses coordonnées lorsque la raquette va vers le haut"""
         self.y -= vitesse
@@ -14,4 +14,3 @@ class Raquette:
         """Change ses coordonnées lorsque la raquette va vers le bas"""
         self.y += vitesse
         self.dy += vitesse
-print("hello!")
