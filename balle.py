92360bfd6cf9404f7099bcec2e204f48c709b501
Lilian <liliannalil347@gmail.com>
2021-07-02 21:59:49 +0200

    Update balle.py

--- balle.py
+++ balle.py
@@ -14,16 +14,16 @@ class Balle:
         self.y=coords[1]
         self.dx=longueurs[0] #largeur de la balle
         self.dy=longueurs[1] #hauteur de la balle
-        self.deplX=4 #déplacement sur l'axe X
-        self.deplY=3 #déplacement sur l'axe Y
+        self.deplX= c.vitesse_balle[0] #déplacement sur l'axe X
+        self.deplY= c.vitesse_balle[1] #déplacement sur l'axe Y
     def show(self, ecran):
         p.draw.rect(ecran, c.couleur_blanc, (self.x, self.y, self.dx, self.dy))
     def deplacement(self):
         if self.y>(c.dimension_ecran[1]-10): #si la balle cogne contre mur bas
-            self.deplY=-3 #déplacement sur l'axe Y
+            self.deplY=-c.vitesse_balle[1] #déplacement sur l'axe Y
         if self.y<0: #contre mur haut
-            self.deplY=3
+            self.deplY= c.vitesse_balle[1] 
         if self.x>(c.dimension_ecran[0]-10):
-            self.deplX=-4
+            self.deplX=-c.vitesse_balle[0] 
         self.x+=self.dx
         self.y+=self.dy
