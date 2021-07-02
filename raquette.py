e72c5b7983afe1bfdbcb5f9ed2a84f3a82c04fbb
Lilian <liliannalil347@gmail.com>
2021-07-02 18:23:44 +0200

    Update raquette.py

--- raquette.py
+++ raquette.py
@@ -1,11 +1,12 @@
 import pygame
+import constantes as c
 class Raquette:
     def __init__(self, coords, longueurs):
         self.x, self.y = coords[0], coords[1]
         self.dx, self.dy = longueurs[0], longueurs[1]
     def show(self, ecran):
         """Affiche la raquette à l'écran"""
-        pygame.draw.rect(ecran, (0,0,0), (self.x,self.y,self.dx,self.dy))
+        pygame.draw.rect(ecran, c.couleur_blanc, (self.x,self.y,self.dx,self.dy))
     def up(self, vitesse):
         """Change ses coordonnées lorsque la raquette va vers le haut"""
         self.y -= vitesse
