b85105d35b6673b0c8d587f9ce0a2326d923fe23
Lilian <liliannalil347@gmail.com>
2021-07-02 18:47:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,18 @@ import balle as b
 pygame.init()
 
 ecran = pygame.display.set_mode(c.dimension_ecran)
-raquette = r.Raquette((0,0),(c.largueur_raquette,c.longueur_raquette))
+raquette = r.Raquette((10,380),(c.largueur_raquette,c.longueur_raquette))
 
 while c.continuer: 
+    pygame.draw.rect(ecran, c.couleur_noire, (0,0,c.dimension_ecran[0],c.dimension_ecran[1])) #Fond
     raquette.show(ecran)
     for event in pygame.event.get():
         if event.type == QUIT:
             c.continuer = False
+        if event.type == KEYDOWN:
+            if event == K_UP:
+                raquette.up(c.vitesse_raquette)
+                print(raquette.y)
     pygame.display.flip()
     
 pygame.quit()
