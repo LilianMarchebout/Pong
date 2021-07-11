b7b289f4be55480965830e2a4994cdcf9ab5e8ac
Lilian <liliannalil347@gmail.com>
2021-07-11 23:31:05 +0200

    Update fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -1,12 +1,18 @@
 import math
+import constantes as c
 
 def balle_raquette(balle, raquette):
     """Retourne si la balle touche la raquette"""
-    return (raquette.x < balle.x < raquette.x+raquette.dx) and (raquette.y < balle.y < raquette.y+raquette.dy)
+    #return (raquette.x < balle.x < raquette.x+raquette.dx) and (raquette.y < balle.y < raquette.y+raquette.dy)
+    return (balle.x < raquette.x+raquette.dx) and (raquette.y < balle.y < raquette.y+raquette.dy)
 
 def frappe(balle, raquette):
     """Retourne le nouveau vecteur vitesse après que la balle ait touché la raquette"""
-    impact_ref_raquette_y = raquette.coords_milieu_raquette()[1]-balle.coords_milieu_balle()[1]
-    angle = math.sin(impact_ref_raquette_y/30*math.pi/2)
-    return -balle.deplX, balle.deplY*angle*math.sqrt(5)
+    #impact_ref_raquette_y = raquette.coords_milieu_raquette()[1]-balle.coords_milieu_balle()[1]
+    a = angle(balle,raquette)
+    return 3*math.sin(180-a), 3*math.cos(180-a)
         
+def angle(balle,raquette):
+    """"Retourne l'angle"""
+    return (90/raquette.dy)*balle.coords_milieu_balle()[1]+(45-(90/raquette.dy)*(raquette.y+raquette.dy))
+
