c966bf37dcb2c0f1b8402f2025ca6c9dbb9475fb
Lilian <liliannalil347@gmail.com>
2021-07-11 16:16:40 +0200

    Create fonctions.py

---
+++ fonctions.py
@@ -0,0 +1,12 @@
+import math
+
+
+def balle_raquette(balle, raquette):
+    """Retourne si la balle touche la raquette"""
+    return (raquette.x < balle.x < raquette.x+raquette.dx) and (raquette.y < balle.y < raquette.y+raquette.dy)
+
+def frappe(balle, raquette):
+    impact_ref_raquette_y = raquette.coords_milieu_raquette()[1]-balle.coords_milieu_balle()[1]
+    angle = math.sin(impact_ref_raquette_y/30*math.pi/2)
+    return -balle.deplX, balle.deplY*angle*math.sqrt(5)
+        
